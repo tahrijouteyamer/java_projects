b5df97d7b4b2040f417671880a3bd19e05ac7377
Amer Tahri-Joutey <125614736+tahrijouteyamer@users.noreply.github.com>
2025-01-05 23:40:23 +0100

    .

--- Main.java
+++ Main.java
@@ -1,30 +1,25 @@
-// Polymorphism / Inheritance (Method Overriding)
-class Animal {
-  void sound() {
-      System.out.println("Some generic animal sound");
-  }
-}
+import java.util.Scanner;
 
-class Dog extends Animal {
-  @Override
-  void sound() {
-      System.out.println("Bark!");
-  }
-}
-
-class Cat extends Animal {
-  @Override
-  void sound() {
-      System.out.println("Meow!");
-  }
-}
 
 public class Main {
-  public static void main(String[] args) {
-      Animal myDog = new Dog();
-      Animal myCat = new Cat();
-    
-      myDog.sound();
-      myCat.sound();
-  }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+    }
 }
